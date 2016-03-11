7f832a64f44426cde5b93b3d595551c0f436f091
Devin <woohoo8987@yahoo.com>
2016-03-10 17:02:18 -0800

    MAGICLY FIXED THE GOOGLE MAP, Added to the blog post
    moo

--- HiddenHaven/src/HiddenHaven/js/contact_me.js
+++ HiddenHaven/src/HiddenHaven/js/contact_me.js
@@ -51,7 +51,7 @@ $(function() {
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     //$('#success > .alert-danger').append("<strong>Sorry " + firstName + " it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:me@example.com?Subject=Message_Me from myprogrammingblog.com;>me@example.com</a> ? Sorry for the inconvenience!");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", something went wrong with the message system.<strong> Please try again later :(");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", the send messages feature isn't currently working.<strong> Please try again later :(");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
